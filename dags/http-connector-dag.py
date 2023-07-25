f14fcdc376bd4eb18069ef0de3a5bb03881f2bfb
Heiser <heiser9295@hotmail.com>
2023-07-25 16:14:21 +0800

    add simple http dag

---
+++ dags/http-connector-dag.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
+import pendulum
+
+
+def my_uri():
+    from airflow.hooks.base import BaseHook
+
+    return BaseHook.get_connection("http_swapi").get_uri()
+
+
+with DAG(
+    dag_id="swapapi-http-connection",
+    description="http connection to swapi",
+    tags=["http"],
+    start_date=pendulum.datetime(2023, 7, 24),
+    schedule=None,
+    catchup=False,
+) as dag:
+    t1 = SimpleHttpOperator(
+        task_id="http",
+        endpoint="planets/1/",
+        method="GET",
+        http_conn_id="http_swapi",
+        log_response=True,
+    )
+    t2 = PythonOperator(task_id="print_uri", python_callable=my_uri)
+    t1 >> t2
